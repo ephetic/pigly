676bc6304250c811c4aba02dc701080f38b2c676
ephetic <bpcleary@gmail.com>
2015-08-04 11:50:59 -0700

    [Auth] Begins refactor of auth system

--- models/user.js
+++ models/user.js
@@ -6,19 +6,27 @@ var UserSchema = db.Schema({
   username: String,
   password: String,
 });
-UserSchema.methods.comparePassword = function(attemptedPassword, callback) {
-  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+UserSchema.methods.comparePassword = function(password, callback) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
     callback(isMatch);
   });
 };
 
+UserSchema.methods.checkPassword = function (password, callback) {
+  bcrypt.compare(password, this.password, callback);
+};
+
 UserSchema.pre('save',function(next){
-  var cipher = Promise.promisify(bcrypt.hash);
-  return cipher(this.password, null, null).bind(this)
-    .then(function(hash) {
+  if(!this.isModified('password')) return next();
+  bcrypt.getSalt(10,function(err,salt){
+    if(err) return next(err);
+    bcrypt.hash(this.password, salt, null, function(err, hash){
+      if(err) return next(err);
       this.password = hash;
+      // this.salt = salt;
       next();
-    });
+    })
+  })
 });
 
 var User = db.model('User', UserSchema);
