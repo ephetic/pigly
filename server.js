3a3d09ab24c1a362687924d8d35905398a21d955
ephetic <bpcleary@gmail.com>
2015-08-04 11:52:34 -0700

    [API] adds  upload to api (see home.js upload() )

--- server.js
+++ server.js
@@ -16,12 +16,11 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(partials());
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/uploads'));
 
 app.param('pic', function(req, res, next, filename){
-  // console.log('pic param', filename, req.method);
   Pic.findOne({filename: filename}, function(err, pic){
     if(!err) req.pic = pic;
     next();
@@ -32,13 +31,14 @@ app.get('/api/pics/:pic', function(req, res, next){
   if(!req.pic) res.send('404');
   var pic = {
     id: req.pic.id,
+    name: req.pic.filename,
     piggies: req.pic.piggies,
     skelies: req.pic.skelies
   }
   res.send(pic);
 })
 
-app.post('/api/pics/:pic', function(req, res, next){
+app.post('/api/pics/:pic', bodyParser.json(), function(req, res, next){
   if(!req.pic) res.setStatus(404).send();
   console.log('post',req.path, req.body);
   if(!req.body.vote) res.setStatus(500).send();
@@ -49,10 +49,18 @@ app.post('/api/pics/:pic', function(req, res, next){
 })
 
 //TODO make this /api/pics call for ajax loading
-app.get('/', function(req,res){
+app.get('/api/pics', function(req,res){
   Pic.find(function(err, pics){
     pics = pics || [];
-    res.render('homepage', {data : pics});
+    pics = pics.map(function(pic){
+      return {
+        id: pic.id,
+        name: pic.filename,
+        piggies: pic.piggies,
+        skelies: pic.skelies
+      }
+    })
+    res.send(pics);
   });
 })
 
@@ -62,21 +70,91 @@ app.get('/capture', function(req,res){
 
 app.post('/capture', upload.single('pic'), function(req,res){
   var file = req.file;
-  console.log(file);
-  var pic = new Pic({
-    filename: file.filename,
-    originalname: file.originalname,
-    encoding: file.encoding,
-    mimetype: file.mimetype,
-    size: file.size,
-    userid: 0, //req.session.userid,
-    caption: 'none', //added later
-  }).save(function(err, pic){
-    console.log('new pic', err, pic);
-  });
+  console.log('got file',file);
+  if(file){
+    var pic = new Pic({
+      filename: file.filename,
+      originalname: file.originalname,
+      encoding: file.encoding,
+      mimetype: file.mimetype,
+      size: file.size,
+      userid: 0, //req.session.userid,
+      caption: 'none', //added later
+    }).save(function(err, pic){
+      console.log('new pic', err, pic);
+    });
+  }
 
-  res.redirect('/');
+  res.end();
 })
+
+// TEST 1
+/* on good form: 
+part --
+part
+Content-Disposition: form-data; name="pic"; filename="healthfitnessrevolution-com.jpg"
+Content-Type: image/jpeg
+
+����JFIFdd��C
+part --
+*/
+// app.post('/capture', function(req, res){
+//   var data = '';
+//   req.on('data', function(chunk){data+=chunk});
+//   req.on('end', function(){
+//     // var boundary = req.headers['content-type'].match(/boundary=(.*)/)[1];
+//     console.log('header',req.headers);
+//     console.log('data', data.slice(0,150));
+//     // var ddata = data.split(boundary).forEach(function(part){
+//     //   console.log('part', part.slice(0,150));
+//     // })
+//   })
+//   res.end();
+// })
+// TEST 2
+// app.post('/capture', upload.single('pic'), function(req, res){
+//   console.log('file', req.file);
+//   res.end();
+// })
+
+app.post('/signin', bodyParser.json(), function(req, res, next){
+  var username = req.body.username,
+      password = req.body.password;
+
+  User.find({username: username}, function(user){
+    if(!user) return next(new Error('No such user'));
+    user.checkPassword(password, function(err, foundUser){
+      if(!foundUser) return next(new Error('No such user'));
+      var token = jwt.encode(user, 'secret');
+      res.json({token: token})
+    })
+  })
+})
+
+app.post('/signup', bodyParser.json(), function(req, res, next){
+  var username = req.body.username,
+      password = req.body.password;
+  User.findOne({username:username}, function(err, user) {
+    if(user) return next(new Error('User already exists!'));
+    new User({username: username, password: password})
+    .save(function(err, user){
+      console.log('created user', err, user);
+    })
+  });
+})
+
+app.get('/signedin', function(req, res, next){
+  var token = req.headers['x-access-token'];
+  if(!token) return next(new Error('No token'));
+  var user = jwt.decode(token, 'secret');
+  User.findOne({username: user.username}, function(err, user){
+    if(user) return res.send(200);
+    if(err) return next(err);
+    res.send(401);
+  })
+})
+
+
 
 var port = process.env.PORT || 8888;
 console.log('Pigly is listening on', port);
